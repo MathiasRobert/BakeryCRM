d4e8d8fdbe78e94e3b696f059add571d4e4e8e31
MathiasRobert <matrobert@ensc.fr>
2019-10-04 14:53:36 +0200

    Display purchases

---
+++ bakery-crm/src/components/Customer/CustomerPurchases.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import {
+    Card,
+    CardHeader,
+    CardContent,
+    Divider,
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableRow
+} from '@material-ui/core';
+import AddPurchase from './AddPurchase';
+
+const CustomerPurchases = (props) => {
+    const { purchases, customerID } = props;
+    console.log(customerID)
+
+    return (
+        <Card>
+            <CardHeader
+                action={
+                    <AddPurchase customerID={customerID} />
+                    // <Button
+                    //     color="primary"
+                    //     size="small"
+                    //     variant="outlined"
+                    // >
+                    //     Add purchase
+                    // </Button>
+                }
+                title="Last 10 purchases"
+            />
+            <Divider />
+            <CardContent>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Product</TableCell>
+                            <TableCell>Price</TableCell>
+                            <TableCell>Date of purchase</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {purchases.map(purchase => (
+                        <TableRow
+                            hover
+                            key={purchase.id}
+                        >
+                            <TableCell>{purchase.productName}</TableCell>
+                            <TableCell>{purchase.price}</TableCell>
+                            <TableCell>{purchase.timestamp 
+                                ? (new Date(purchase.timestamp)).toLocaleDateString() + ' ' + (new Date(purchase.timestamp)).toLocaleTimeString() 
+                                : 'Unknown'}
+                            </TableCell>
+                        </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>                        
+            </CardContent>
+        </Card>
+    );
+};
+
+export default CustomerPurchases;
