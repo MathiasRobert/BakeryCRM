6d33886957d4588a167d809a63b27436a4804f5f
MathiasRobert <matrobert@ensc.fr>
2019-10-04 02:39:15 +0200

    App name redirect to the dashboard

--- bakery-crm/src/components/Layout/NavBar.js
+++ bakery-crm/src/components/Layout/NavBar.js
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import { NavLink } from 'react-router-dom';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -10,6 +11,8 @@ const useStyles = makeStyles(theme => ({
   },
   title: {
     flexGrow: 1,
+    textDecoration: 'none',
+    color: 'inherit',
   },
 }));
 
@@ -20,7 +23,7 @@ function NavBar() {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6" className={classes.title}>
+          <Typography component={NavLink} to="/" variant="h6" className={classes.title}>
             BakeryCRM
           </Typography>
         </Toolbar>
